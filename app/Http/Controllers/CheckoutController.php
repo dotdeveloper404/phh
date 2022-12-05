c01df548f6feb99d6f5a726b0be1bb3320b16c2a
dotdeveloper404 <dot.developer404@gmail.com>
2022-12-05 22:44:24 +0500

    newtest

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -14,7 +14,7 @@ class CheckoutController extends Controller
     public function index()
     {
 
-        
+        //test
         $cart = Cart::getContent();
         if ($cart->isEmpty()) {
             return redirect()->route('cart.list')->with('error', 'Add products to your cart first.');
